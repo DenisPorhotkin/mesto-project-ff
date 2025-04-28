b991c4f8b000af2c2be851baa3b36c3e36cc779c
Porhotkin <porhotkin@yandex.ru>
2025-04-28 22:54:21 +0300

    первая сборка до ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function TemplateCard(index) {
+
+    if (!initialCards[index]) return;
+    const cardTemplate = document.querySelector('#card-template').content;
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+
+    card.querySelector('.card__image').src = initialCards[index].link;
+    card.querySelector('.card__image').alt = initialCards[index].name;
+    card.querySelector('.card__title').textContent = initialCards[index].name;
+    card.querySelector('.card__delete-button').addEventListener('click', () => {
+        deleteCard(card);
+    });
+    placesList.append(card);
+}
 
 // @todo: Функция создания карточки
+function addCards() {
+
+    initialCards.forEach((_, index) => TemplateCard(index));
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(item) {
+
+    item.style.transition = 'opacity 0.5s ease';
+    item.style.opacity = '0';
+    setTimeout(() => {item.remove();}, 500);
+}
 
 // @todo: Вывести карточки на страницу
+addCards();
